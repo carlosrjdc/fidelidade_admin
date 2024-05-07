94a2f1723bc08b2906dff52cd447973588544997
Carlos <uppaplicativo@gmail.com>
2024-05-06 23:35:18 -0300

    first commit

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image"
 import header from "../../assets/header.jpg"
-import { FaUserPlus } from "react-icons/fa";
+import { FaPowerOff } from "react-icons/fa6";
 import { Button } from "../ui/button"
 
 
@@ -16,8 +16,8 @@ export default function Header() {
               <h1 className="font-bold">CARLOS ROSA</h1>
             </div>
             <div>
-              <Button className="flex gap-2">
-                <FaUserPlus size={20} />
+              <Button className="flex gap-2 bg-red-400">
+                <FaPowerOff size={20} />
                 Sair
               </Button>
             </div>
